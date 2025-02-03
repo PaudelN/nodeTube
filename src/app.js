2e3495dec720172d95022fba3419e836d1c07627
Nukesh <nukeshpaudel99@gmail.com>
2025-02-03 15:20:13 +0545

    utils and custom err/api

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+//app.use() is generally for the middlewares
+
+//to connect the frontend url with our backend
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    // methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+//to allow data from frontend such as forms,json data etc
+//to allow data such as images we use multer
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+//to allow data from url
+//The url encoded for space (_) is %20 and so on every character have a unique url encoded
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+//to store the allowed files and folders from any type of data in public asset
+app.use(express.static("public")); //public is the name of the folder created before, which means i wanna keep files or folders in this folder
+
+//use of cookieParser in backend is to allow the backend engineer to access the cookies set in the user's browser and also allow to set the new cookie for the user.
+app.use(cookieParser())
+
+
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -2,29 +2,41 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8001, () =>
+      console.log(`\ SERVER IS RUNNING AT PORT ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection FAILED !!", err);
+  });
 
-//Second approach for connecting database is directly in the index.js file(Less preferable)
-// import express from "express";
-// const app = express();
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("Error in db connection", error);
-//       throw error;
-//     });
+/*-------------------------------------------------------------------------------------------------------
+Second approach for connecting database is directly in the index.js file(Less preferable)
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error in db connection", error);
+      throw error;
+    });
 
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on PORt ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error connecting database", error);
-//     throw error;
-//   }
-// })();
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on PORt ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting database", error);
+    throw error;
+  }
+})();
+
+*/
